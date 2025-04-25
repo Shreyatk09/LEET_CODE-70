b4b80f9b3088174e0cb6a9d761ff1cf18093673c
Shreyatk09 <162786344+Shreyatk09@users.noreply.github.com>
2025-04-25 13:14:28 +0530

    Update smallerNumbersThanCurrent

--- Array/smallerNumbersThanCurrent.cpp
+++ Array/smallerNumbersThanCurrent.cpp
@@ -1,3 +1,20 @@
+//BruteForce Approach
+vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> result(n);
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            if (nums[j] < nums[i])
+                count++;
+        }
+        result[i] = count;
+    }
+
+    return result;
+}
+
 //optimized problm
 class Solution {
 public:
